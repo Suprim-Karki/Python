8bbf3c11141838a6b8ada9392180789f1a4232af
Suprim-Karki <shupreem1911@gmail.com>
2025-04-16 21:03:31 +0545

    Task 4 completed

--- practice.py
+++ practice.py
@@ -70,8 +70,8 @@ Call the function and display the result.
 def count_vowels(word):
     vowels = "aeiouAEIOU"
     count = 0
-    for "your code here":
-        if "your code here":
+    for letter in word:
+        if letter in vowels:
             count += 1
     return count
 
